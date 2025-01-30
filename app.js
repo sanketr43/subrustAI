da1f29318c64f4702a24108a98e77da0498f38fa
Sanket Raut <sanketr43@gmail.com>
2025-01-30 14:07:54 +0530

    Update app.js

--- app.js
+++ app.js
@@ -83,20 +83,29 @@ Highcharts.chart("container", {
               // Example data - replace with actual data retrieval logic
               const items = [
                 {
-                  name: "Item 1",
-                  image_url: "https://placehold.jp/100x100.png",
+                  name: "管理栄養士監修 成長サプリ にこにこカ...",
+                  image_url:
+                    "https://thumbnail.image.rakuten.co.jp/@0_mall/sukusuku-noppokun/cabinet/07385567/imgrc0087994984.jpg?_ex=128x128",
                 },
                 {
-                  name: "Item 2",
-                  image_url: "https://placehold.jp/100x100.png",
+                  name: "【ネコポス送料無料】アンクレット 足...",
+                  image_url:
+                    "https://thumbnail.image.rakuten.co.jp/@0_mall/vox/cabinet/79/d-mkb-11332_1.jpg?_ex=128x128",
                 },
                 {
-                  name: "Item 3",
-                  image_url: "https://placehold.jp/100x100.png",
+                  name: "日傘 クールプラス UV遮熱遮光コンパク...",
+                  image_url:
+                    "https://thumbnail.image.rakuten.co.jp/@0_mall/higasa/cabinet/items/1702/1702_01.jpg?_ex=128x128",
                 },
                 {
-                  name: "Item 4",
-                  image_url: "https://placehold.jp/100x100.png",
+                  name: "全品10%OFF●送料無料 スニーカー シュ...",
+                  image_url:
+                    "https://thumbnail.image.rakuten.co.jp/@0_mall/adachi-jozo/cabinet/jisya/08852640/imgrc0082454263.jpg?_ex=128x128",
+                },
+                {
+                  name: "もっとしっとり150g &クレンジング150g...",
+                  image_url:
+                    "https://thumbnail.image.rakuten.co.jp/@0_mall/xsyce/cabinet/compass1661401360.jpg?_ex=128x128",
                 },
               ];
 
